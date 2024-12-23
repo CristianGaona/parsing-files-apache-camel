d9b08fee42c5e6068f51cf8e06531902bd3fc9c9
Cristian Gaona <crgaonas24@gmail.com>
2024-12-23 15:15:31 -0500

    Added initial commits

---
+++ src/main/java/com/crisda24/neoris/parsingfilesapachecamel/ParsingFilesApacheCamelApplication.java
@@ -0,0 +1,13 @@
+package com.crisda24.neoris.parsingfilesapachecamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingFilesApacheCamelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParsingFilesApacheCamelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crisda24/neoris/parsingfilesapachecamel/ParsingFilesApacheCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisda24.neoris.parsingfilesapachecamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParsingFilesApacheCamelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
